a58643d694359e6e0575a0ffd65e14ca22367420
Seppe Gadeyne <seppe@fushia.be>
2020-01-14 12:24:54 +0100

    Update

--- bash-scripting-opdracht1.sh
+++ bash-scripting-opdracht1.sh
@@ -1,20 +1,21 @@
 #!/bin/bash 
 
+[[ $# -ne 1 ]] && { echo "Geef exact één parameter!"; exit 1; }
+[[ ! -d "$1" ]] && { echo "Dit is geen geldige directory"; exit 1; }
+
 leesbare_entries=0
 uitvoerbare_entries=0
-leesbare_uitvoerbare_entries=0
-
-[ ! -d "$1" ] && { echo "Dit is geen geldige directory"; exit 1; }
+leesbare_uitvoerbare_entries=0                                                                                                                                                                                                                
 
 for item in $(ls -1 $1)
 do
-    #echo "Test op ${item}"
-    [ -r "${item}" ] && leesbare_entries=$((leesbare_entries + 1))
-    [ -x "${item}" ] && uitvoerbare_entries=$((uitvoerbare_entries + 1))
-    if [ -r "${item}" ] && [ -x "${item}" ]                                                                                                                                                                        
+    echo "Test op ${item}"
+    [[ -r "${item}" ]] && leesbare_entries=$((leesbare_entries + 1))
+    [[ -x "${item}" ]] && uitvoerbare_entries=$((uitvoerbare_entries + 1))
+    if [[ -r "${item}" ]] && [[ -x "${item}" ]]
     then
         leesbare_uitvoerbare_entries=$((leesbare_uitvoerbare_entries + 1))
-    fi
+    fi  
 done
 
 echo "Het aantal leesbare entries: ${leesbare_entries}"
